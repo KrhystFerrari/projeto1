1448ae8224dda3e698074a7fa4db9b779da63e82
Krhystofferson <krhys_ferrari@hotmail.com>
2022-04-14 12:02:05 -0300

    commit1

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,13 @@
-
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Krhys',
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Krhyst Ferrari',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Krhyst Ferrari',
     })
