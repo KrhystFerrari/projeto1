3be21bb96784b3ff172cacb7b265e33ff6fb63af
Krhystofferson <krhys_ferrari@hotmail.com>
2022-05-03 15:27:40 -0300

    commit

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,17 @@
 from distutils.command.upload import upload
+from email.policy import default
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -18,6 +27,11 @@ class Recipe(models.Model):
     crated_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category =
-    autor =
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
