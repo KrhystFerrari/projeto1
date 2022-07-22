071862d4065dac5cf634c92f19881eda0f5ac29e
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-21 22:43:02 -0300

    comit

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -29,9 +29,9 @@ class RecipeViewsTest(TestCase):
         author = User.objects.create_user(
             first_name='user',
             last_name='name',
-            username='uername',
+            username='username',
             password='123456',
-            email='username@user.com',
+            email='username@email.com',
         )
         recipe = Recipe.objects.create(
             category=category,
@@ -47,7 +47,15 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
